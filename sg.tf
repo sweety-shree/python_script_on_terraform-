bacff4d2eaf47c549609028ac2711c9aa67e2b38
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-03 16:00:37 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,33 @@
+# Creating Security Group for EC2 Instance
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  # Inbound Rules
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound Rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Instance SG"
+  }
+}
