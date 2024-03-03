3583c7cda8d386ade80157d6b8c5f4ea2cc92ee7
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-03 15:59:09 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,7 @@
+# Creating file for Outputs
+
+# Output of the 1st Instance Public IP
+output "public_ip_address_1" {
+  description = "Public IP of 1st Instance"
+  value       = aws_instance.python.public_ip
+}
