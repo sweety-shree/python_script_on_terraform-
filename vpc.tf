ed805dc0258daa3413e38ba6cca68bfc0355bfce
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-04 20:17:27 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 #creating virtual private cloud VPC
 resource "aws_vpc" "vpc" {
-  cidr_block       = var.vpc_cidr
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
     Name    = "VPC"
