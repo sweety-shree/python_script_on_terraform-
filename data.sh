04acfc86f0989fd43048bfeb0e98d830c14dd7d5
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-04 20:37:44 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/03Aziz02/fish-py.git
-cd fish-py
+git clone https://github.com/03Aziz02/MyFuel-py.git
+cd MyFuel-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
