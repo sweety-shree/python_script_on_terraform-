41bc17ec696115d5193827e5273eafbf147b971e
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-05 14:46:02 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/03Aziz02/MyFuel-py.git
-cd MyFuel-py
+git clone https://github.com/03Aziz02/Medical-Insurance-py.git
+cd Medical-Insurance-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
