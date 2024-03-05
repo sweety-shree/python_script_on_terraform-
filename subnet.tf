72105441f972480f9c1fb967cb8e17bd8ad721b5
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-05 11:15:47 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-1a"
   tags = {
     Name = "Web Subnet 1"
   }
