d0802c018b49d0a8335e7d63de56e91679f8167a
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-05 14:14:25 +0530

    Update route.tf

--- route.tf
+++ route.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
