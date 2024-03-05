93c94e99469a54b0b3e4891107cc9d1740116135
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-05 16:03:34 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/03Aziz02/Medical-Insurance-py.git
-cd Medical-Insurance-py
+git clone https://github.com/03Aziz02/flight-perdiction-py.git
+cd flight-perdiction-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
