d52cd2014ce46bcddee1775b037e185d4ea07476
sweety-shree <152768615+sweety-shree@users.noreply.github.com>
2024-03-19 17:03:56 +0530

    data.sh

--- data.sh
+++ data.sh
@@ -2,6 +2,6 @@
 sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/sweety-shree/Portfolio.git
-cd Portfolio
+cd portfolio
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
