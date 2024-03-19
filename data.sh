14b97321613a56a42dd2f79637030a58f05ef7b6
sweety-shree <152768615+sweety-shree@users.noreply.github.com>
2024-03-19 16:49:03 +0530

    data.sh

--- data.sh
+++ data.sh
@@ -2,6 +2,6 @@
 sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/sweety-shree/Portfolio.git
-cd flight-perdiction-py
+cd Portfolio
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
